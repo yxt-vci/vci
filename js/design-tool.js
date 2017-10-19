5b8805b63e6b449ee70f7d7866a47a63f7a3c52b
yxt <756894229@qq.com>
2017-10-19 18:00:09 +0800

    设计工具功能界面

--- js/design-tool.js
+++ js/design-tool.js
@@ -1,3 +1,27 @@
 /**
  * Created by Administrator on 2017/10/18.
  */
+$(function() {
+    var arr=[];
+    $(".content .left-tool-box .drap").draggable({
+        appendTo: ".drop-box",
+        helper: "clone"
+    });
+    $(".common-box .toolAdd").droppable({
+        activeClass: "ui-state-default",
+        hoverClass: "ui-state-hover",
+        accept: "*",
+        drop: function( event, ui ) {
+            var dom = ui.draggable[0];
+            var url = $(dom).find("img").attr("src");
+            var text = $(dom).find("div").text();
+            if(arr.indexOf(text) == -1){
+                var html = '<div class="fl toolList">' +
+                    '<img src="'+url+'" class="tool-logo-wh" alt=""/>' +
+                    '<div>'+text+'</div></div>';
+                $(".drop-box").append(html);
+                arr.push(text);
+            }
+        }
+    })
+});
